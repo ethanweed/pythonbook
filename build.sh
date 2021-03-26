14d97eee3e6c1878d5e39c41926a24095020ad4f
Ethan Weed <weed.ethan@gmail.com>
2021-03-26 20:39:11 +0100

    re-organized jupyterbook structure

---
+++ build.sh
@@ -0,0 +1,4 @@
+
+
+jupyter-book build /Users/ethan/Documents/GitHub/pythonbook/Chapters/ --path-output /Users/ethan/Documents/GitHub/pythonbook/Book --config /Users/ethan/Documents/GitHub/pythonbook/yaml/_config.yml --toc /Users/ethan/Documents/GitHub/pythonbook/yaml/_toc.yml 
+
