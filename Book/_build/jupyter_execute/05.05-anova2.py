47ec3dcd927a35c50edd15b05edb142dafc382bc
Ethan Weed <weed.ethan@gmail.com>
2022-07-07 13:18:24 +0200

    auto-updated with build.sh

--- Book/_build/jupyter_execute/05.05-anova2.py
+++ Book/_build/jupyter_execute/05.05-anova2.py
@@ -34,8 +34,49 @@ pd.crosstab(index=df["drug"], columns=df["therapy"],margins=False)
 round(df.groupby(['therapy', 'drug'])[['mood_gain']].mean(),2)
 
 
-# In[ ]:
+# Now, this output shows a cross-tabulation of the group means for all possible combinations of the two factors (e.g., people who received the placebo and no therapy, people who received the placebo while getting CBT, etc). However, we can also construct tables that ignore one of the two factors. That gives us output that looks like this:
+
+# In[3]:
 
 
+round(df.groupby(['therapy'])[['mood_gain']].mean(),2)
 
 
+# In[4]:
+
+
+round(df.groupby(['drug'])[['mood_gain']].mean(),2)
+
+
+# But of course, if we can ignore one factor we can certainly ignore both. That is, we might also be interested in calculating the average  mood gain across all 18 participants, regardless of what drug or psychological therapy they were given:
+
+# In[5]:
+
+
+round(df['mood_gain'].mean(),2)
+
+
+# At this point we have 12 different sample means to keep track of! It is helpful to organise all these numbers into a single table, which would look like this:
+# 
+# |         |no therapy |CBT  |total |
+# |:--------|:----------|:----|:-----|
+# |         |no therapy |CBT  |total |
+# |placebo  |0.30       |0.60 |0.45  |
+# |anxifree |0.40       |1.03 |0.72  |
+# |joyzepam |1.47       |1.50 |1.48  |
+# |total    |0.72       |1.04 |0.88  |
+
+# Now, each of these different means is of course a sample statistic: it's a quantity that pertains to the specific observations that we've made during our study. What we want to make inferences about are the corresponding population parameters: that is, the true means as they exist within some broader population. Those population means can also be organised into a similar table, but we'll need a little mathematical notation to do so. As usual, I'll use the symbol $\mu$ to denote a population mean. However, because there are lots of different means, I'll need to use subscripts to distinguish between them. 
+# 
+# Here's how the notation works. Our table is defined in terms of two factors: each row corresponds to a different level of Factor A (in this case `drug`), and each column corresponds to a different level of Factor B (in this case `therapy`). If we let $R$ denote the number of rows in the table, and $C$ denote the number of columns, we can refer to this as an $R \times C$ factorial ANOVA. In this case $R=3$ and $C=2$. We'll use lowercase letters to refer to specific rows and columns, so $\mu_{rc}$ refers to the population mean associated with the $r$th level of Factor A (i.e. row number $r$) and the $c$th level of Factor B (column number $c$).[^notesubscript] So the population means are now written like this:
+# 
+# [^notesubscript]: The nice thing about the subscript notation is that generalises nicely: if our experiment had involved a third factor, then we could just add a third subscript. In principle, the notation extends to as many factors as you might care to include, but in this book we'll rarely consider analyses involving more than two factors, and never more than three. 
+
+# |         |no therapy |CBT        |total |
+# |:--------|:----------|:----------|:-----|
+# |placebo  |$\mu_{11}$ |$\mu_{12}$ |      |
+# |anxifree |$\mu_{21}$ |$\mu_{22}$ |      |
+# |joyzepam |$\mu_{31}$ |$\mu_{32}$ |      |
+# |total    |           |           |      |
+
+# 
